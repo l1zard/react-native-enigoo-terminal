a926d32883e7f4226efbf8527e1bb79ddbec70cc
Karel Andres <andres@enigoo.cz>
2023-03-31 10:52:30 +0200

    passivate

--- android/src/main/java/com/enigoo/terminal/csob/Connection.java
+++ android/src/main/java/com/enigoo/terminal/csob/Connection.java
@@ -9,43 +9,45 @@ import java.io.IOException;
 public class Connection extends AsyncTask<Void, Void, Boolean> {
 
 
-    public Payment payment;
+  public Payment payment;
 
-    private final byte[] message;
-    private String type;
-    private String orderId;
+  private final byte[] message;
+  private String type;
+  private String orderId;
 
-    public Connection(Payment payment, byte[] message, String type, String orderId) {
-        this.payment = payment;
-        this.message = message;
-        this.type = type;
-        this.orderId = orderId;
-    }
+  public Connection(Payment payment, byte[] message, String type, String orderId) {
+    this.payment = payment;
+    this.message = message;
+    this.type = type;
+    this.orderId = orderId;
+  }
 
-    @Override
-    protected Boolean doInBackground(Void... voids) {
-        try {
-            ProcessMessage processMessage = new ProcessMessage(payment, message, type, orderId);
+  @Override
+  protected Boolean doInBackground(Void... voids) {
+    try {
+      ProcessMessage processMessage = new ProcessMessage(payment, message, type, orderId);
+      int count = 0;
+      if (type != null && type.equals("PASSIVATE")) {
+        for (Thread t : Thread.getAllStackTraces().keySet()) {
+          if(t instanceof ConnectionThread) count++;
+          if (t instanceof ConnectionThread && !((ConnectionThread) t).isPassivating()) {
+            ((ConnectionThread) t).setPassivating(true);
+            SocketConnection.interrupt();
+          }
 
-            if (type != null && type.equals("PASSIVATE")) {
-                for (Thread t : Thread.getAllStackTraces().keySet()) {
-                    if (t instanceof ConnectionThread && !((ConnectionThread)t).isPassivating()) {
-                        ((ConnectionThread)t).setPassivating(true);
-                        SocketConnection.interrupt();
-                    }
-
-                }
-            }else{
-                ConnectionThread t = new ConnectionThread(processMessage);
-                t.start();
-            }
-
-            return true;
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+      }
+      if (count == 0) {
+        ConnectionThread t = new ConnectionThread(processMessage);
+        t.start();
+      }
 
-        return false;
+      return true;
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    return false;
+  }
 }
